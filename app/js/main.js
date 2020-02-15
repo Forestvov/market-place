981d4c242ee1399f075f00f32067dce16e934de8
Forestvov <kukva2019@mail.ru>
2020-02-15 21:14:29 +1000

    Your Followers Feed

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,12 @@ $(function(){
         prevArrow: '<button type="button" class="arrw-box slick-prev lnr lnr-chevron-left"></button>',
         nextArrow: '<button type="button" class="arrw-box slick-next lnr lnr-chevron-right"></button>'
     });
-
+    $('.uFollowes__inner').slick({
+        prevArrow: '<button type="button" class="arrw-box slick-prev lnr lnr-chevron-left"></button>',
+        nextArrow: '<button type="button" class="arrw-box slick-next lnr lnr-chevron-right"></button>',
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+    
     var mixer = mixitup('.newrelease__inner');
 });
